317131252ce2bbba553914e9275ec996d87aef37
Juan Peri <eternoperegrino@gmail.com>
2016-09-01 17:47:22 +0200

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM quay.io/getapp/docker-backup
+
+MAINTAINER Juan Peri
+
+RUN gem install elasticsearch-extensions
---
+++ models/elasticsearch.rb
@@ -0,0 +1,18 @@
+Model.new(:elasticsearch, 'Elasticsearch Backup') do
+  database Elasticsearch do |db|
+    db.url     = ENV['ELASTIC_URL']
+    db.indices = ENV['BACKUP_INDICES']
+    db.size    = ENV['BACKUP_SIZE'] || 500
+    db.scroll  = ENV['BACKUP_SCROLL'] || '10m'
+  end
+
+  store_with S3 do |s3|
+    s3.access_key_id = ENV['AWS_ACCESS_KEY_ID']
+    s3.secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
+    s3.region = ENV['AWS_REGION']
+    s3.bucket = ENV['AWS_BUCKET']
+    s3.path = ENV['AWS_ELASTIC_PATH']
+  end
+
+  compress_with Gzip
+end
